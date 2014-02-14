6cbc77bda4f564d26e26b4fd6a8c05f40abba027
Airat Shigapov <airat@shigapov.me>
2014-02-14 12:09:44 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestTaskSubmitter::Application.config.secret_key_base = '3a660acf1871b1fb8ac70c35e1e3392071ef17ea9e71f81efdfc84226da0219a91ba392e399422caf018fdda894f564ab9844110459bc8086589a3a13c727f11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestTaskSubmitter::Application.config.session_store :cookie_store, key: '_test-task-submitter_session'
