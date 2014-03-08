e37e03389cfe976f0c2f7d829571c3a0d0be552e
Vladimir Mikhailov <vladimir.mikhailov@flatstack.com>
2014-03-08 08:36:17 +0400

    Remove email column from submission model

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -24,6 +24,6 @@ class SubmissionsController < ApplicationController
   private
 
   def submission_params
-    params.require(:submission).permit(:email, :source_code)
+    params.require(:submission).permit(:source_code)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140214094336) do
+ActiveRecord::Schema.define(version: 20140308043246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
   create_table "submissions", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
-    t.string   "email"
     t.text     "source_code"
     t.datetime "created_at"
     t.datetime "updated_at"
